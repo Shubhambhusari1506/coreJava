f2ec11a274e7e8f4c1b9a177504f2a945a906ef0
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-21 11:46:04 +0530

    Added:calculating wages for month

--- EmpWageUC1.java
+++ EmpWageUC1.java
@@ -4,27 +4,31 @@ public  class EmpWageUC1 {
 			int WAGE_PER_HOUR=20;			
 			int  IS_FULL_TIME=1;
 			int IS_PART_TIME=2;
+			int DAYS_IN_MONTH=20;
 				//Variables 
 				int emp_hrs=0;
-				int daily_wage=0;
-
+				int monthly_wage=0;
+				int days=0;
 				//computation
+				while (days !=DAYS_IN_MONTH){
 			int   empCheck= (int) Math.floor(Math.random()*10)%3;
-				switch (empCheck) 
-					{
-						
-					case 1: emp_hrs=8;
-					
-					
-					case 2:
-					
-						emp_hrs=4;
-					
-					case 3: 
-						emp_hrs=4;
-					
-				default:daily_wage=emp_hrs*WAGE_PER_HOUR;
-					     System.out.println("Employee wage" +daily_wage);
+				if  (empCheck==IS_FULL_TIME)
+						{
+						 
+						 emp_hrs=8;
+						}
+						else if  (empCheck==IS_PART_TIME) 
+							{
+							emp_hrs=4;
+							} 
+						else 
+							{
+								emp_hrs=0;
+							}
+					days=days +1;
+					monthly_wage=monthly_wage+emp_hrs*WAGE_PER_HOUR;
+					}
+						System.out.println("Monthly employee wage" +monthly_wage);
 		}
-	}
+	
 }
