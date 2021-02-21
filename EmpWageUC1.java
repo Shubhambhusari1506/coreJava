55829363047aff1025b2625def3a7f6b69d5e59e
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-21 10:02:52 +0530

    Added:Part time wage

--- EmpWageUC1.java
+++ EmpWageUC1.java
@@ -3,23 +3,27 @@ public  class EmpWageUC1 {
 			//constant
 			int WAGE_PER_HOUR=20;			
 			int  IS_FULL_TIME=1;
+			int IS_PART_TIME=2;
 				//Variables 
 				int emp_hrs=0;
 				int daily_wage=0;
 
 				//computation
 			int  empCheck= (int) Math.floor(Math.random()*10)%3;
-				if (empCheck==IS_FULL_TIME) {
+				if (empCheck==IS_FULL_TIME) 
+					{
 						
-					System.out.println("Employee is present");
-						emp_hrs=8;
+					emp_hrs=8;
 
-				}
-					else {
-						
-					System.out.println("Employee is absent");
-						emp_hrs=0;
-				}
+					}
+					else if (empCheck==IS_PART_TIME)
+					{
+						emp_hrs=4;
+					}
+					else
+					{	
+						emp_hrs=4;
+					}
 				
 						daily_wage=emp_hrs*WAGE_PER_HOUR;
 					System.out.println("Employee wage" +daily_wage);
