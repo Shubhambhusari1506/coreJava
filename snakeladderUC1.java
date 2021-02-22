08ee6b53e15e34dbfba1022b9f3c9070711b352d
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-22 16:35:37 +0530

    Added:single player and position of player.

---
+++ snakeladderUC1.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class snakeladderUC1 {
+
+      public static void main (String [] args)
+
+
+         {
+            //constants
+
+               int winning_pos1=100;
+               int starting_pos=0;
+
+
+              char player1;
+
+            System.out.println("welcome to snake and ladder game");
+            
+            System.out.println("Player Position======>" +starting_pos);
+
+   		}
+
+}
+
+
