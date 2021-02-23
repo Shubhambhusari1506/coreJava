df9aff22ac3e2cd90dc5e46eb7dbe218273f5e56
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-23 15:53:44 +0530

    Added: no play ,snake, ladder

--- snakeladderUC1.java
+++ snakeladderUC1.java
@@ -15,37 +15,37 @@ public class snakeladderUC1 {
          {
             //variables 
 							
-						int currentposition = 0;
+				int currentposition = 0;
 
 			
 					
-					//computation
+				//computation
 
-					int rollDice;
+				int rollDice;
 
                rollDice =  (int) Math.floor(Math.random() * 10 ) % 6 + 1;
 					System.out.println("dice number:   " +rollDice);
 					currentposition += rollDice;
 
 
-					int checkplay = (int) Math.floor(Math.random() * 10) % 3;
-							switch (checkplay)
-								{
-									case ladder:
-												rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-												currentposition += rollDice;
-												break;
+				int checkplay = (int) Math.floor(Math.random() * 10) % 3;
+					switch (checkplay)
+						{
+						case ladder:
+						rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+						currentposition += rollDice;
+						break;
 									
 
-									case snake:
-											  rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-                                    currentposition += rollDice;
-                                    break;
+						case snake:
+						rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+                  currentposition += rollDice;
+                  break;
 
-									default:
-											//no play
-											currentposition += 0;
-								}
+						default:
+		    			//no play
+						currentposition += 0;
+						}
 
 						System.out.println("current position : "  +currentposition);
 
