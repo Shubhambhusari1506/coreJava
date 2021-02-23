7c93ba91253282563cabff36c1bfe2f32cf601f8
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-23 08:07:56 +0530

    Added:NO play,ladder,snake

--- snakeladderUC1.java
+++ snakeladderUC1.java
@@ -1,21 +1,56 @@
 
-import java.util.Scanner;
 public class snakeladderUC1 {
+				
+				
+				
+		public  static final int start_pos = 0;
+		public static final int ladder = 0;
+		public static final int snake = 1;
+
+
 
       public static void main (String [] args)
 
 
          {
-            //constants
+            //variables 
+							
+						int currentposition = 0;
 
-               int winning_pos1=100;
-               int starting_pos=0;
+			
+					
+					//computation
+
+					int rollDice;
+
+               rollDice =  (int) Math.floor(Math.random() * 10 ) % 6 + 1;
+					System.out.println("dice number:   " +rollDice);
+					currentposition += rollDice;
 
 
-              int Diceroll = (int) (Math.random() *10) % 6;
+					int checkplay = (int) Math.floor(Math.random() * 10) % 3;
+							switch (checkplay)
+								{
+									case ladder:
+												rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+												currentposition += rollDice;
+												break;
+									
 
-            System.out.println("number comes on dice after rolling~~" +Diceroll);
+									case snake:
+											  rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+                                    currentposition += rollDice;
+                                    break;
 
+									default:
+											//no play
+											currentposition += 0;
+								}
+
+						System.out.println("current position : "  +currentposition);
+
+
+              
    		}
 
 }
